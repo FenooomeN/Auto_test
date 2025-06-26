5ca230c6ac7daa7ac6d79d7f09314e5c82ca0425
FenooomeN <FenoooomeN@gmail.com>
2025-06-26 14:45:54 +0300

    pet_store

---
+++ lib/constant/pet_shop_const.py
@@ -0,0 +1,2 @@
+class PetStatus:
+    SOLD = "sold"
---
+++ tests/pet_store/test_pet.py
@@ -0,0 +1,13 @@
+from lib.app.pet_store.petstore import PetStore
+from lib.constant.pet_shop_const import PetStatus
+
+pet_store = PetStore()
+
+class TestPet():
+
+    def test_get_sold_pets(self, status=PetStatus.SOLD):
+        'Проверяем что методом GET pet_find_by_status со параметром Sold, получаем только проданых петомцев'
+        resp = pet_store.get_pet_find_by_status(status=status)
+        for r in resp.json():
+            assert r['status'] == PetStatus.SOLD
+
